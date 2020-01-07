717246fb41680e87c3fb4eb3639f67f0a29132f0
fuliming <fuliming@xiaomi.com>
2020-01-07 16:43:14 +0800

    init

---
+++ src/main/java/com/frida/sixfoot/sixfoot/SixfootApplication.java
@@ -0,0 +1,13 @@
+package com.frida.sixfoot.sixfoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixfootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixfootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frida/sixfoot/sixfoot/SixfootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frida.sixfoot.sixfoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixfootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
